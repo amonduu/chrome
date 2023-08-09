bdd38aa13a4aa9b95032cf248950606b48342abd
amonduu <89570663+amonduu@users.noreply.github.com>
2023-08-09 21:56:34 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+const player = {
+    name: "nico", 
+    sayHello: function(name){
+        console.log("hello " + name)
+    },
+};
+
+console.log(player.name);
+player.sayHello("kim");
+player.sayHello("susu");
